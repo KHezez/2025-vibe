c2a173b11606b11b9a410f537d1e363ef184520d
KHezez <soomin37183718@gmail.com>
2025-07-23 10:18:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import streamlit as st
-st.title('나의 첫 웹서비으ㅓ 어ㅓ러ㅏ')
-name = st.text_input('이름을 입력해주세요:::')
-menu = st.selectbox('좋아하는 음식을 선택해주세요:', ['망고빙수','아몬드봉봉','벌레'])
-if st.button ('인사말 생성'):
-  st.write(name+'님!'+menu+'를 좋아하네요')
-  
+import random
+
+# 간단한 타이틀 + CSS로 색상 강조
+st.markdown(
+    """
+    <style>
+    .title {
+        font-size: 2.5em;
+        color: #ff914d;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 30px;
+        text-shadow: 2px 2px 8px #ffd8b5;
+    }
+    </style>
+    <div class="title">🍚 오늘 뭐 먹지? 메뉴 추천기</div>
+    """,
+    unsafe_allow_html=True
+)
+
+menu_list = [
+    "김치찌개", "된장찌개", "비빔밥", "치킨", "피자", "라면", "삼겹살", "돈까스",
+    "떡볶이", "순두부찌개", "초밥", "제육볶음", "불고기", "냉면", "파스타", "햄버거"
+]
+
+if st.button("메뉴 추천받기! 🍴"):
+    menu = random.choice(menu_list)
+    st.success(f"오늘의 메뉴는... **{menu}** 어떰? 😎")
+
+st.caption("by monday. 오늘도 든든하게 먹어라~")
